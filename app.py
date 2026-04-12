3914e771b9a1331fbea61891f9832e8280e4df20
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-04-12 22:30:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -473,16 +473,15 @@ else:
                     """, unsafe_allow_html=True)
                     
                     with cols_top[i]:
-                        # Usamos el contenedor nativo de Streamlit con borde
-                           with st.container(border=True):
-                        #with st.container(border=True):
-                            # Inyectamos CSS solo para el color del borde de este contenedor específico
-                            #st.markdown(f"""<style>
-                                #[data-testid="stContainer"]:has(> div > div > div > .marco-{i}) {{
-                                    #border: 2px solid {color_fondo} !important;
-                                    #background-color: #D3D3D3;
-                               # }}
-                           # </style><div class="marco-{i}"></div>""", unsafe_allow_html=True)
+                        st.markdown(f"""
+                            <div style="
+                                background-color: #D3D3D3; 
+                                border: 6px solid #FFFFFF; 
+                                border-radius: 20px; 
+                                padding: 20px; 
+                                color: #000000;
+                            ">
+                        """, unsafe_allow_html=True)
                             
                             st.metric(label=f"Ahorro en {dias_totales} días", value=f"{ahorro_total} €", delta=f"Opción {i+1}", delta_color=color_metrica)
                             st.metric(label="Estimación Ahorro Anual (IVA inc.)", value=f"{ahorro_anual} €", delta_color=color_metrica)
@@ -497,7 +496,7 @@ else:
                                 {texto_boton}
                             </div>
                             </a>''', unsafe_allow_html=True)
-                           
+                            st.markdown("</div>", unsafe_allow_html=True)
    
                             
             st.divider()
