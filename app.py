804ef778f6ee26d254231f0cbf26a9924b4a4165
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-04-14 15:51:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -335,7 +335,7 @@ if os.path.exists("Logo_Energetika.png"):
 
 st.markdown("### ⚡ Comparador Energetika de Facturas Eléctricas")
 
-excel_path = "tarifas_companias.xlsx"
+excel_path = "tarifas_companias.xls"
 
 
 if not os.path.exists(excel_path):
